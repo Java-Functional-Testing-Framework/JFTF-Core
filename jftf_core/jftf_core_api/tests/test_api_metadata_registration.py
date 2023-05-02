034fda986a73afc6186ff481a170b92a468dd105
Decebal <radulescudecebal@outlook.com>
2023-05-02 22:25:14 +0300

    SNAPSHOT-1.1/Experimental - Added related logs to test case metadata API test case

--- jftf_core/jftf_core_api/tests/test_api_metadata_registration.py
+++ jftf_core/jftf_core_api/tests/test_api_metadata_registration.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
@@ -8,6 +9,7 @@ from django.contrib.auth.models import User
 class TestCaseMetadataAPITestCase(APITestCase):
 
     def setUp(self):
+        self.test_logger = getLogger(f"Test logger {TestCaseMetadataAPITestCase.__name__}")
         # Create a test user
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         # Log in the test user
@@ -23,24 +25,28 @@ class TestCaseMetadataAPITestCase(APITestCase):
 
     def Test_create_test_case_metadata(self):
         # Create a valid test case metadata object
-
+        self.test_logger.info("Attempting to create valid test case metadata object")
         url = reverse('test-case-metadata-list')
         response = self.client.post(url, self.duplicate_metadata, format='json')
 
+        self.test_logger.info(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(TestCaseMetadata.objects.count(), 1)
         self.assertEqual(TestCaseMetadata.objects.get().testName, "test1")
 
     def Test_create_test_case_metadata_duplicate_entry(self):
+        self.test_logger.info("Test case metadata duplicate entry test")
         url = reverse('test-case-metadata-list')
         response = self.client.post(url, self.duplicate_metadata, format='json')
 
+        self.test_logger.info(response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {'non_field_errors': [
             'Duplicate entry with the same test version, test path, and test name already exists']})
 
     def Test_create_test_case_metadata_invalid_test_path(self):
         # Attempt to create a test case metadata object with an invalid test path
+        self.test_logger.info("Test case metadata with invalid test path")
         data = {
             "testName": "test1",
             "featureGroup": "feature1",
@@ -52,6 +58,7 @@ class TestCaseMetadataAPITestCase(APITestCase):
         url = reverse('test-case-metadata-list')
         response = self.client.post(url, data, format='json')
 
+        self.test_logger.info(response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('non_field_errors', response.data.keys())
         self.assertEqual(response.data['non_field_errors'][0],
@@ -61,6 +68,7 @@ class TestCaseMetadataAPITestCase(APITestCase):
     def Test_create_test_case_metadata_invalid_test_group_and_test_name(self):
         # Attempt to create a test case metadata object with test group and test name that do not correspond
         # to the provided values
+        self.test_logger.info("Test case metadata with invalid group and test name")
         data = {
             "testName": "test2",
             "featureGroup": "feature1",
@@ -72,6 +80,7 @@ class TestCaseMetadataAPITestCase(APITestCase):
         url = reverse('test-case-metadata-list')
         response = self.client.post(url, data, format='json')
 
+        self.test_logger.info(response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('non_field_errors', response.data.keys())
         self.assertEqual(response.data['non_field_errors'][0],
