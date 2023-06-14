ce1797f3f3a714d2974dd0bc4cc9923edc784799
Decebal <radulescudecebal@outlook.com>
2023-06-15 01:28:11 +0300

    SNAPSHOT-1.1/Experimental - Implemented resource only route in /api/test-case to call the execute_jftf_test_case task

--- jftf_core/jftf_core_api/views/test_case.py
+++ jftf_core/jftf_core_api/views/test_case.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
+from ..tasks import execute_jftf_test_case
 from .pagination import ContentRangeHeaderPagination
 from ..models import TestCases
 from ..serializers import TestCaseSerializer, TestCaseAdminSerializer
@@ -15,6 +19,32 @@ class TestCaseModelViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['executed']
     pagination_class = ContentRangeHeaderPagination
+
+    @action(detail=True, methods=['post'])
+    def execute(self, request, pk=None):
+        try:
+            # Retrieve the TestCase instance
+            test_case = self.get_object()
+
+            # Serialize the TestCase instance
+            serializer = self.get_serializer(test_case)
+            serialized_data = serializer.data
+
+            # Access the metaData field value from the serialized data
+            metaData_value = serialized_data['metaData']
+
+            # Get the jar path from the metaData serialized data
+            jar_path = metaData_value['testPath']
+
+            # Trigger the celery task
+            result = execute_jftf_test_case.delay(jar_path)
+
+            # Return the task ID or any other response as needed
+            return Response({'task_id': result.id}, status=status.HTTP_200_OK)
+        except TestCases.DoesNotExist:
+            return Response({'error': 'TestCase not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class TestCaseAdminModelViewSet(viewsets.ModelViewSet):
