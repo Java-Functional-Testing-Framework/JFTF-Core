cf64f7fd4220c4c850ba75d0d7fb5dd1833e10ac
Decebal <radulescudecebal@outlook.com>
2023-06-16 22:30:06 +0300

    SNAPSHOT-1.1/Experimental - Fix TaskResultViewSet configuration

--- jftf_core/jftf_core_api/views/task_result.py
+++ jftf_core/jftf_core_api/views/task_result.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from django_celery_results.models import TaskResult
+from django_filters.rest_framework import DjangoFilterBackend
 from ..serializers import TaskResultSerializer
 from .pagination import ContentRangeHeaderPagination
 
@@ -7,5 +8,7 @@ from .pagination import ContentRangeHeaderPagination
 class TaskResultViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = TaskResult.objects.all()
     serializer_class = TaskResultSerializer
-    pagination_class = ContentRangeHeaderPagination
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['task_id']
+    pagination_class = ContentRangeHeaderPagination
