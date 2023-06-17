5c2f94e4e5be39e98d05042f6075db89e029b91c
Decebal <radulescudecebal@outlook.com>
2023-06-17 23:00:51 +0300

    SNAPSHOT-1.1/Experimental - Extend /api/test-case-metadata/ filterset fields

--- jftf_core/jftf_core_api/views/test_case_metadata.py
+++ jftf_core/jftf_core_api/views/test_case_metadata.py
@@ -28,7 +28,7 @@ class TestCaseMetadataModelViewSet(viewsets.ModelViewSet):
     serializer_class = TestCaseMetadataSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['testName', 'testGroup', 'featureGroup']
+    filterset_fields = ['testName', 'testGroup', 'featureGroup', 'testPath', 'testVersion']
     pagination_class = ContentRangeHeaderPagination
 
     @extend_schema(
